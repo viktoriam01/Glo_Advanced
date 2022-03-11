107f4b8904aa7cda45876d779548fc9c1a3d4087
Viktoria <aislingdar@gmail.com>
2022-03-11 19:54:06 +0800

    Урок 19 -  исправлено в соотвествии с комментариями

--- script.js
+++ script.js
@@ -3,39 +3,51 @@ const day = document.querySelector('.day');
 const time = document.querySelector('.time');
 const timer = document.querySelector('.timer');
 
-let now = new Date()
+
+const runTimer = () => {
+        
+      let now = new Date()
+
+      const showGreating = () => {
+         
+         if (now.getHours()  < 0) return greating.textContent = 'ERROR'
+
+         if (now.getHours() < 6 ) return greating.textContent = 'Доброй ночи'
+            
+         if (now.getHours() < 12) return greating.textContent = 'Доброе утро'
+                  
+         if (now.getHours() < 18) return greating.textContent = 'Добрый день'
+                     
+         if (now.getHours() < 24) return greating.textContent = 'Добрый вечер'
+                     
+      }
 
 
-const showGreating = () => {
-   console.log(now.getHours());
-  
-   if (now.getHours() >= 0 && now.getHours() < 6 ) {
-      greating.textContent = 'Доброй ночи'
-      } else if(now.getHours() >= 6 && now.getHours() < 12) {
-            greating.textContent = 'Доброе утро'
-         } else if(now.getHours() >= 12 && now.getHours() < 18) {
-               greating.textContent = 'Добрый день'
-            } else if(now.getHours() >= 18 && now.getHours() < 24) {
-               greating.textContent = 'Добрый вечер'
-               }
+      showGreating()
+
+
+      let timeNow = now.toLocaleTimeString('en')
+      time.textContent = `Текущее время: ${timeNow}`
+
+
+      // const weekday = ["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
+      // let d = dayOfWeek[now.getDay()]
+      // day.textContent = `Сегодня: ${d}`
+
+
+      let dayOfWeek = (now).toLocaleDateString('ru-RU', {weekday: 'long'})
+      dayOfWeek = dayOfWeek[0].toUpperCase() + dayOfWeek.slice(1)
+      day.textContent = `Сегодня: ${dayOfWeek}`
+
+
+      let nextYear  = now.getFullYear() + 1
+      let dateStop = new Date(`1 january ${nextYear}`).getTime()  
+      let dateNow = new Date().getTime()
+      let timeRemaining = (dateStop - dateNow) / 1000
+      let days = Math.floor(timeRemaining / 60 / 60 /24)      
+      timer.textContent = `До нового года осталось ${days} дней`
+
+
 }
 
-showGreating()
-
-
-let timeNow = now.toLocaleTimeString('en')
-time.textContent = `Текущее время: ${timeNow}`
-
-
-const weekday = ["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
-let d = weekday[now.getDay()]
-day.textContent = `Сегодня: ${d}`
-
-
-let nextYear  = now.getFullYear() + 1
-
-let dateStop = new Date(`1 january ${nextYear}`).getTime()  
-let dateNow = new Date().getTime()
-let timeRemaining = (dateStop - dateNow) / 1000
-let days = Math.floor(timeRemaining / 60 / 60 /24)      
-timer.textContent = `До нового года осталось ${days} дней`
+setInterval(runTimer, 1000)
